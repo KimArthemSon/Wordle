58639d3239c10e603d5dc419146bae39dc2e0203
Kim_Arthem <sonkimarthem@gmail.com>
2024-10-31 16:10:59 +0800

    WordleProject

---
+++ Words.js
@@ -0,0 +1,38 @@
+export class words{
+     word() {
+const wordList = [
+    "apple", "brick", "crane", "dream", "flute", "grape", "heart", "joker", "lemon",
+    "magic", "night", "ocean", "plant", "quake", "robin", "stone", "table", "unite",
+    "vivid", "whale", "xenon", "youth", "zebra", "anger", "badge", "chair", "dance",
+    "eagle", "fable", "globe", "haste", "icier", "jolly", "knife", "latch", "mouse",
+    "noble", "orbit", "pearl", "quest", "racer", "swing", "tiger", "usher", "vocal",
+    "waltz", "xylem", "yacht", "zesty", "alley", "baker", "candy", "diner", "eager",
+    "fresh", "grasp", "hound", "input", "joint", "koala", "leash", "mango", "nerdy",
+    "olive", "plaza", "quilt", "rally", "sheep", "tread", "urban", "vigor", "whisk",
+    "yummy", "zesty", "bloom", "crown", "debit", "elbow", "fancy", "gamer", "hatch",
+    "ideal", "jewel", "knack", "laser", "march", "nerve", "oasis", "piano", "quark",
+    "refit", "sheen", "tango", "under", "vices", "weigh", "axion", "bison", "crush",
+    "dozen", "equip", "flame", "glint", "hover", "icily", "joust", "kites", "laser",
+    "medal", "novel", "other", "prism", "quark", "roast", "scoop", "track", "unzip",
+    "vapid", "watch", "yodel", "zippy", "ample", "brave", "charm", "dough", "equip",
+    "flare", "gleam", "hasty", "image", "jumpy", "knock", "light", "mirth", "needy",
+    "opine", "plumb", "quash", "racer", "sheep", "tiger", "ultra", "vapid", "whirl",
+    "yield", "zippy", "adore", "blend", "caste", "drain", "eerie", "flick", "gleef",
+    "homer", "input", "joint", "kinky", "leafy", "merit", "nerve", "ocean", "paved",
+    "quilt", "rally", "stack", "toast", "upset", "vapor", "weary", "axis", "bloom",
+    "cloud", "delta", "eagle", "fresh", "globe", "heart", "ivory", "jolly", "knife",
+    "laser", "magic", "noble", "orbit", "plaza", "quilt", "river", "sheep", "tiger",
+    "ultra", "vigor", "watch", "xenon", "yacht", "zesty", "angel", "bring", "crate",
+    "daisy", "enter", "flora", "gravy", "hatch", "irate", "jumpy", "kinda", "laser",
+    "minor", "nerdy", "olive", "punch", "quirk", "robot", "sunny", "taste", "upend",
+    "valid", "widen", "yield", "zippy", "alien", "beard", "clash", "demon", "eject",
+    "frown", "glare", "hobby", "ivory", "jewel", "knock", "laser", "merry", "nifty",
+    "ocean", "place", "quilt", "robot", "shake", "treat", "unite", "vocal", "whale",
+    "yacht", "zesty", "agent", "binge", "crown", "delta", "equip", "flare", "gleam",
+    "horse", "index", "joint", "knife", "laser", "maple", "nerve", "olive", "plumb",
+    "quilt", "rebel", "sheen", "trial", "usual", "vivid", "wacky", "yield", "zippy"
+];
+    return wordList[Math.floor(Math.random() * wordList.length)];
+
+    }
+}
---
+++ function.js
@@ -0,0 +1,129 @@
+import { words } from "./Words.js";
+
+const word = new words();
+
+const inpt = document.querySelectorAll('.inpt');
+let Word_to_guest = word.word().toUpperCase();
+const Myguest = [];
+
+let row = 0;
+let endCol =5;
+let score=0;
+
+console.log(Word_to_guest);
+document.addEventListener('keyup', (event)=>{
+    console.log(Word_to_guest);
+    if(event.key=='Enter'){
+       if(Myguest.length==endCol){
+          Check_win();
+       }
+    }
+    else if(event.key=='Backspace'){
+        if(Myguest.length>row){
+           Myguest.pop();
+           fillBoard();
+        }
+    }
+    else if(isLetter(event.key)){
+        if(Myguest.length<endCol){
+          Myguest.push(event.key.toUpperCase());
+          fillBoard();
+        }
+    }
+})
+
+function Check_win(){
+    let count =row;
+    let j=row;
+    let greens = [];
+    let Myanswer = [];
+    for(let i=0;i<Word_to_guest.length;i++){
+            greens.push(Word_to_guest.charAt(i));
+            Myanswer.push(Myguest[j++]);
+    }  
+   j=row;
+
+    for(let i=0;i<Word_to_guest.length;i++){
+        if(Word_to_guest.charAt(i)===Myanswer[i]){
+            count++;
+            greens[i]='';
+            Myanswer[i]='';
+            inpt[j].classList.add('green');
+        }
+        j++;
+    }  
+
+    j=row;
+    
+    for(let i=0;i<Word_to_guest.length;i++){
+         if(Myanswer[i]!==''&&greens.includes(Myanswer[i])){
+             for(let j1 =0;j1<greens.length;j1++){
+                if(greens[j1]==Myanswer[i]){
+                    greens[j1]='';
+                    Myanswer[i]='';
+                    break;
+                }
+             }
+            inpt[j].classList.add('yellow');
+        }
+        j++;
+    }
+    
+    j=row;
+
+    for(let i=0;i<Word_to_guest.length;i++){
+        if(Myanswer[i]!==''){
+            inpt[j].classList.add('black');
+        }
+        j++;
+    }
+
+    if(count==endCol){
+       win();
+    }else if(endCol==30){
+      fillBoard(resetGame());
+    }else{
+        row+=5;
+        endCol+=5;
+    }
+}
+
+function win(){ 
+     score++;
+   document.getElementById("score").textContent = "Score: "+score;
+   setTimeout(()=>{
+    fillBoard(resetGame());
+   },200)
+}
+const aplha = "abcdefghijklmnopqrstuvwxyz";
+function isLetter(letter){
+      letter = letter.toLowerCase();
+      return aplha.includes(letter);
+}
+function resetGame(){
+    let c = Myguest.length;
+  for(let i=0;i<c;i++){
+    Myguest.pop();
+  }
+  for(let i=0;i<inpt.length;i++){
+     inpt[i].className = '';
+     inpt[i].classList.add('inpt');
+  }
+ 
+  endCol = 5;
+  row=0;
+  Word_to_guest = word.word().toUpperCase();
+} 
+
+
+function fillBoard(){
+    for(let i=0;i<inpt.length;i++){
+        inpt[i].textContent='';
+    }
+    for(let i=0;i<Myguest.length;i++){
+         inpt[i].textContent = Myguest[i];
+    }
+}
+
+
+
